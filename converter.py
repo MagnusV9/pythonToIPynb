97ccacc74f9146ab001665ee8455344be57f883f
Magnus <100690976+MagnusV9@users.noreply.github.com>
2023-03-24 21:14:00 +0100

    Update converter.py

--- converter.py
+++ converter.py
@@ -1,18 +1,26 @@
 import sys
+import re
 import nbformat
-from nbformat.v4 import new_code_cell, new_notebook
+from nbformat.v4 import new_code_cell, new_markdown_cell, new_notebook
 
 def convert_to_ipynb(py_file, ipynb_file):
     with open(py_file, 'r') as f:
         content = f.read()
 
-    # Split the Python file into a list of code blocks
-    code_blocks = content.split('\n\n')
+    # Split the content using triple quotes
+    blocks = re.split("'''", content)
 
-    # Create a new Jupyter Notebook and add code cells
+    # Create a new Jupyter Notebook
     nb = new_notebook()
-    for block in code_blocks:
-        nb.cells.append(new_code_cell(block))
+
+    # Iterate through the blocks, adding either Markdown or code cells
+    for i, block in enumerate(blocks):
+        if i % 2 == 0:  # Code block
+            if block.strip():
+                nb.cells.append(new_code_cell(block.strip()))
+        else:  # Markdown block
+            if block.strip():
+                nb.cells.append(new_markdown_cell(block.strip()))
 
     # Save the Jupyter Notebook
     with open(ipynb_file, 'w') as f:
@@ -27,4 +35,5 @@ if __name__ == '__main__':
     ipynb_file = sys.argv[2]
 
     convert_to_ipynb(py_file, ipynb_file)
+
 
